b1f05e1521a95394df78121dacaac65a3f138553
Dide van Berkel <dide.vanberkel@gmail.com>
2016-05-28 13:57:39 +0200

    Initial Commit

---
+++ going-local/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  going-local
+//
+//  Created by Dide van Berkel on 28-05-16.
+//  Copyright © 2016 Dide van Berkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
